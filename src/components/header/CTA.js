ccb7589c41e304e7cac32bf316b2d9a15fd54066
studentjsw <hema47173@gmail.com>
2024-03-25 16:42:52 +0530

    first commit

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -6,7 +6,7 @@ const CTA = () => {
     <div className='cta'>
       {/* <a href={CV} download className='btn'>Resume</a> */}
       {/* <a href='https://drive.google.com/file/d/1IUeCngMxk27QdMR4fWJ2nSDmu8Bt9Z8J/view?usp=sharing' target="_blank" rel='noreferrer' className='btn'>Resume</a> */}
-      <a href='https://drive.google.com/file/d/174h_WJ5d6NpbuGNsW9YcqUt7kKRz9RQb/view'target="_blank" rel='noreferrer' className='btn'>Resume</a>
+      <a href='https://drive.google.com/file/d/17Gb90UhkrBVIHCFiMxoe1oI3c_2sBgz4/view?usp=sharing'target="_blank" rel='noreferrer' className='btn'>Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
 
     </div>
